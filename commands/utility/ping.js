9a0b1900570caf6583c638c5bd641b9274c2f361
Midnight Doggo <git@midnightdoggo19.com>
2025-02-25 17:19:20 +0000

    ping command utilizes health endpoint

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,4 +1,5 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+require('dotenv').config();
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -6,6 +7,16 @@ module.exports = {
 		.setDescription('Check if the bot is responsive!')
 	.setContexts(0, 1),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		await interaction.deferReply({ flags: 64 });
+
+		const status = await fetch(`http://${process.env.IP}:${process.env.PORT}/health`);
+
+		const statusEmbed = new EmbedBuilder()
+			.setTitle('Bot Status')
+			.setDescription(status.join('\n'))
+			.setColor(0x00AE86)
+			.setTimestamp();
+
+	await interaction.editReply({ embeds: [statusEmbed], flags: 64 });
 	},
 };
