c4ca4fc7471085a0cd2b744228a38e43eb96cdb9
Midnight Doggo <git@midnightdoggo19.com>
2025-02-25 19:12:23 +0000

    moved health endpoint to api

--- index.js
+++ index.js
@@ -204,7 +204,7 @@ app.use((req, res, next) => {
 app.use(express.static('public'));
 app.set('trust proxy', 1);
 
-app.get('/health', rootLimiter, (req, res) => {
+app.get('/api/health', rootLimiter, (req, res) => {
     res.json({
         status: 'ok',
         uptime: process.uptime(),
